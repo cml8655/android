7cd371d3d88685a9d390da173cd325f3ef161a79
cml8655 <222222>
2014-09-20 19:08:14 +0800

    费的身份水电费

--- AuthenticatorActivity/src/com/example/android/samplesync/activities/InviteContactActivity.java
+++ AuthenticatorActivity/src/com/example/android/samplesync/activities/InviteContactActivity.java
@@ -22,20 +22,22 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 /**
- * Activity to handle the invite-intent. In a real app, this would look up the user on the network
- * and either connect ("add as friend", "follow") or invite them to the network
+ * Activity to handle the invite-intent. In a real app, this would look up the
+ * user on the network and either connect ("add as friend", "follow") or invite
+ * them to the network
  */
 public class InviteContactActivity extends Activity {
-    private static final String TAG = "InviteContactActivity";
+	private static final String TAG = "InviteContactActivity";
 
-    private TextView mUriTextView;
+	private TextView mUriTextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.invite_contact_activity);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.invite_contact_activity);
 
-        mUriTextView = (TextView) findViewById(R.id.invite_contact_uri);
-        mUriTextView.setText(getIntent().getDataString());
-    }
+		mUriTextView = (TextView) findViewById(R.id.invite_contact_uri);
+		// mUriTextView.setText(getIntent().getDataString());
+		mUriTextView.setText("¹þ¹þ¹þ sb");
+	}
 }
